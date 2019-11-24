bd09b5c158cdc8dce8059df18fc558079ae7f416
Griggorii <40373174+Griggorii@users.noreply.github.com>
2019-11-24 21:28:44 +0800

    Update Install_nemo_fix_type_find_file.sh

--- Install_nemo_fix_type_find_file.sh
+++ Install_nemo_fix_type_find_file.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
- 
-sudo tar xvpfj nemo.tar.bz2 -C / && notify-send -i info "Nemo find type file by Griggorii@gmail.com"
+
+tar cvpjf nemo_original.tar.bz2 /usr/bin/nemo /usr/lib/x86_64-linux-gnu/libexempi.so.3 /usr/lib/x86_64-linux-gnu/libexempi.so.3.4.5  
+cat << EOF >restore_original_nemo_in_sudo.sh
+sudo rm /usr/lib/x86_64-linux-gnu/libexempi.so.3 && sudo rm /usr/lib/x86_64-linux-gnu/libexempi.so.3.4.5 && sudo tar xvpfj nemo_original.tar.bz2 -C / && notify-send -i info "Setting restore nemo good ! Настройки восстановлены ! donate support https://money.yandex.ru/to/410014999913799"
+EOF
+chmod +x restore_original_nemo_in_sudo.sh
+sudo tar xvpfj nemo.tar.bz2 -C / && notify-send -i info "Nemo find type file by Griggorii@gmail.com restart session please ! donate support https://money.yandex.ru/to/410014999913799"
